805d03c1aeba98899d2bcdb2e4b6b29b6f454e6e
Rahul Sharama <ravigeneralstore161@gmail.com>
2024-05-28 21:15:22 +0530

    Getting hold of response

--- lib/widgets/new_item.dart
+++ lib/widgets/new_item.dart
@@ -19,13 +19,13 @@ class _NewItemState extends State<NewItem> {
   var _enteredQuantity = 0;
   var _enteredCategory = categories[Categories.vegetables]!;
 
-  void _saveItem() {
+  void _saveItem() async {
     if (_formKey.currentState!.validate()) {
       _formKey.currentState!.save();
       final url = Uri.https(
           'shoppingfirebase-98fc2-default-rtdb.firebaseio.com',
           'shopping-list.json');
-      http.post(
+      final response = await http.post(
         url,
         headers: {
           'Content-Type': 'application/json',
@@ -38,6 +38,13 @@ class _NewItemState extends State<NewItem> {
           },
         ),
       );
+      if (!context.mounted) {
+        return;
+      }
+      debugPrint(response.statusCode.toString());
+      debugPrint(response.body);
+
+      Navigator.of(context).pop();
     }
   }
 
